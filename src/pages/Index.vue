a413ca81e296ccff1e19679af2ad6f4a0db79eb0
Diogo Assiz Menon <dmenon@ciandt.com>
2018-09-11 16:44:26 -0300

    [diogo] Teste bluetooth

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <q-page class="flex flex-center">
     <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <button type="button" @click="onButtonClick">Teste</button>
   </q-page>
 </template>
 
@@ -9,6 +10,36 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  methods: {
+    onButtonClick() {
+      console.log('Requesting Bluetooth Device...');
+      navigator.bluetooth.requestDevice(
+        {filters: [{services: ['battery_service']}]})
+      .then(device => {
+        console.log('Connecting to GATT Server...');
+        return device.gatt.connect();
+      })
+      .then(server => {
+        console.log('Getting Battery Service...');
+        return server.getPrimaryService('battery_service');
+      })
+      .then(service => {
+        console.log('Getting Battery Level Characteristic...');
+        return service.getCharacteristic('battery_level');
+      })
+      .then(characteristic => {
+        console.log('Reading Battery Level...');
+        return characteristic.readValue();
+      })
+      .then(value => {
+        let batteryLevel = value.getUint8(0);
+        console.log('> Battery Level is ' + batteryLevel + '%');
+      })
+      .catch(error => {
+        console.log('Argh! ' + error);
+      });
+    }
+  }
 }
 </script>
